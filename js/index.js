cf3d349427965dd551311a981a82452f909f522b
Bossola-Yannick <bossolay@hotmail.fr>
2025-04-07 16:04:05 +0200

    feat: first commit and project initiation

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let script;
+// mise en place de tout les click
+$("#levelUpTurret").on("click", function () {
+  let level = "level de la tourelle";
+  let nameTurret = "nom de la tourelle: ketchuprelle";
+  levelUp(level, nameTurret);
+});
---
+++ js/turrets.js
@@ -0,0 +1,7 @@
+let levelUp = (level, name) => {
+  console.log("level + pts générés");
+};
+
+let priceUp = () => {
+  console.log("prix celon level");
+};
