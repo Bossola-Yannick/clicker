b50064d9816be4e6df762bbbdb71b92be4538cfa
James Sanchez <james.sanchez@laplateforme.io>
2025-04-11 10:23:44 +0200

    change: added turret addEventListener to clickEvent.js and removed it from turrets.js

--- js/clickEvent.js
+++ js/clickEvent.js
@@ -16,3 +16,24 @@ $("#upgradeNbPerso").on("click", function () {
     upgradeNbPerso();
   }
 });
+
+// Element boutons
+const turretKetchup = document.getElementById("ketchup");
+const turretMayo = document.getElementById("mayo");
+const turretBbq = document.getElementById("bbq");
+
+turretKetchup.addEventListener("click", () => {
+  fries = parseInt(localStorage.getItem("fries"));
+  const value = turretKetchup.getAttribute("value");
+  buyTurret(ketchup, fries, value);
+});
+turretMayo.addEventListener("click", () => {
+  fries = parseInt(localStorage.getItem("fries"));
+  const value = turretMayo.getAttribute("value");
+  buyTurret(mayo, fries, value);
+});
+turretBbq.addEventListener("click", () => {
+  fries = parseInt(localStorage.getItem("fries"));
+  const value = turretBbq.getAttribute("value");
+  buyTurret(bbq, fries, value);
+});
